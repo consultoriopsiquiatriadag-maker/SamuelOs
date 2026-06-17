aaac6131ea455e3f0f4d7c9155603357133fe250
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-06-17 15:30:49 -0300

    fase 21: historias sociales

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v25-fase20b';
+const CACHE_VERSION = 'samuel-app-v26-fase21';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
