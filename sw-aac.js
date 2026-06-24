34921f4142d1beb6ca230d2356df1a0fba90e4ff
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-06-24 08:39:05 -0300

    Fase 30: seccion Que es el Mundial (5 tarjetas didacticas)

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v34-fase29';
+const CACHE_VERSION = 'samuel-app-v35-fase30';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
