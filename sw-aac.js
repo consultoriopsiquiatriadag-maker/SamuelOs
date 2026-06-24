b746215a546bb32dabb4fdefe0ae782803674c7d
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-06-24 15:07:00 -0300

    Fase 44: historia social 'Usar el baño' para apoyar el control de esfínteres

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v46-fase43';
+const CACHE_VERSION = 'samuel-app-v47-fase44';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
