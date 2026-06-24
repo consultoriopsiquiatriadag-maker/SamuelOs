9660e7544e4790289d159cc6cf988fe37b05bdf3
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-06-24 19:24:31 -0300

    Fase 56: foto de Samuel también en el avatar de child.html

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v63-foto-samuel';
+const CACHE_VERSION = 'samuel-app-v64-foto-samuel-child';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
