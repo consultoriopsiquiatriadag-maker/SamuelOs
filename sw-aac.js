c31cf4536f0d0858b617755ac7b61f4ec7fe2d97
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-06-24 08:43:02 -0300

    Fase 31: minijuego Encontra la bandera

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v35-fase30';
+const CACHE_VERSION = 'samuel-app-v36-fase31';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
