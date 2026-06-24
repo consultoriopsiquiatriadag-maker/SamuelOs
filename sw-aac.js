0b2075ed293a7a4be458341b80e33c3c1cc2dd49
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-06-24 12:00:48 -0300

    Fases 28-40: Mundial, fixture, bandera, penales, camionero, conectar animales, basquet

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v41-fase39';
+const CACHE_VERSION = 'samuel-app-v42-fase40';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
