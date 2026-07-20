c7d881e06a287093854986b678cff82d00f22c44
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-07-20 10:24:34 -0300

    feat: mochila dia banner + horario editable desde panel adulto (v80)

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v79-escuela-organizo-calmo';
+const CACHE_VERSION = 'samuel-app-v80-mejoras-menores';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
