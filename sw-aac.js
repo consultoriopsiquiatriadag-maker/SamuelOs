d89dd10e1cff8b6b2dbe385a56c2f1d6b218d0e2
Dr. Pedro Dagnino <consultoriopsiquiatriadag@gmail.com>
2026-07-22 11:19:10 -0300

    feat: hub SVG home cards + Música al nivel raíz + Desafíos en Jugar (v81)

--- sw-aac.js
+++ sw-aac.js
@@ -1,5 +1,5 @@
 /* Aplicación de Samuel - Service Worker */
-const CACHE_VERSION = 'samuel-app-v80-mejoras-menores';
+const CACHE_VERSION = 'samuel-app-v81-hub-svg';
 const CACHE_NAME = CACHE_VERSION;
 
 const ASSETS = [
